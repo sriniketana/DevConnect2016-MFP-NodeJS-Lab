9b1a7084ac5f97cff0110e8822a1b740e7d8caf5
Carlos Santana <csantana23@gmail.com>
2016-01-24 21:06:16 -0500

    initial commit

---
+++ IC2016Chat/www/js/app.js
@@ -0,0 +1,114 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+
+//var localDB = new PouchDB('todos', {adapter: 'websql'});
+
+angular.module('starter', ['ionic', 'starter.controllers','starter.services', 'ngCordova.plugins.camera','angularMoment','ngStorage'])
+.constant('AppConfig',{
+  // change this url to match your bluemix application route that you selected when you deployed the app
+  appServer: 'https://ionic.mybluemix.net',
+  // Important do not use localhost when using ios or android hybrid app
+  //appServer: 'http://10.0.1.17:6001',
+  localhostPort: '6001',
+  remoteDBUrl: 'https://97c9a4fe-847d-4bd4-bcdd-c671a3141882-bluemix.cloudant.com/photos_db'
+})
+.run(function($ionicPlatform, MFPClientPromise) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    } 
+     MFPClientPromise.then(function(){WL.Logger.ctx({pkg: 'io.ionic'}).debug('mfp and ionic are ready, safe to use WL.* APIs');});
+  
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.chat', {
+    url: '/chat',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/chat.html',
+        controller: 'ChatCtrl'
+      }
+    }
+  })
+  
+  .state('app.photos', {
+    url: '/photos',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/photos.html',
+        controller: 'PhotoCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/chat');
+})
+
+.filter('timeAgo', function () {
+    return function (date) {
+        if (!date) return;
+        return moment(date, 'x').fromNow();
+    }
+})
+
+.factory('MFPClientPromise', function($q){
+  /* Setup a Promise to allow code to run in other places anytime after MFP CLient SDK is ready
+     Example: MFPClientPromise.then(function(){alert('mfp is ready, go ahead and use WL.* APIs')});
+  */
+  return window.MFPClientDefer.promise;
+});
+
+window.Messages = {
+  // Add here your messages for the default language.
+  // Generate a similar file with a language suffix containing the translated messages.
+  // key1 : message1,
+};
+
+window.wlInitOptions = {
+  // Options to initialize with the WL.Client object.
+  // For initialization options please refer to IBM MobileFirst Platform Foundation Knowledge Center.
+};
+
+window.MFPClientDefer = angular.injector(['ng']).get('$q').defer();;
+window.wlCommonInit = window.MFPClientDefer.resolve;
+window.MFPClientDefer.promise.then(function wlCommonInit(){
+  // Common initialization code goes here or use the angular service MFPClientPromise
+  
+  console.log('MobileFirst Client SDK Initilized');
+  mfpMagicPreviewSetup();
+ });
+
+function mfpMagicPreviewSetup(){
+  var platform;
+  //nothing to see here :-), just some magic to make ionic work with mfp preview, similar to ionic serve --lab
+  if(WL.StaticAppProps.ENVIRONMENT === 'preview'){
+    //running mfp preview (MBS or browser)
+    platform = WL.StaticAppProps.PREVIEW_ENVIRONMENT === 'android' ? 'android' : 'ios';
+    if(location.href.indexOf('?ionicplatform='+platform) < 0){
+      location.replace(location.pathname+'?ionicplatform='+platform);
+    }
+  } 
+}
+
---
+++ IC2016Chat/www/js/controllers.js
@@ -0,0 +1,91 @@
+angular.module('starter.controllers', [])
+
+.controller('ChatCtrl', function ($scope, $ionicModal, $localStorage, $sessionStorage, Camera, FakeChat, SocketIO, randomAvatar, serverHost) {
+	//var ChatManager = FakeChat;
+	var ChatManager = SocketIO;
+	$scope.isWebView = ionic.Platform.isWebView();
+  $scope.$storage = $scope.isWebView ? $localStorage : $sessionStorage;
+  $scope.posts = ChatManager.posts;
+  
+	function addPost(message, img) {
+		ChatManager.add({
+			message: message ? message : null,
+			img: img ? img : null,
+			timestamp: new Date().getTime(),
+      user: $scope.$storage.handle,
+      avatar: $scope.$storage.avatar
+		});
+	}
+  
+	$scope.add = function (message) {
+		addPost(message);
+		$scope.message = null; // pretty things up
+	};
+
+	$scope.takePicture = function () {
+		Camera.takePicture().then(function (photo) {
+			addPost(null, photo);
+		});
+	};
+
+})
+
+.controller('PhotoCtrl', function($scope, $ionicPlatform, PouchSvc ) {
+  
+   $scope.todos = [];
+
+/*
+  // Initialize the database.
+	$ionicPlatform.ready(function() {
+		PouchSvc.initDB();
+
+		// Get all birthday records from the database.
+		PouchSvc.getAllPhotos().then(function(photos) {
+			$scope.todos = photos;
+		});
+	});
+
+
+*/
+  
+  
+})
+
+.controller('AppCtrl', function($scope, $ionicModal, $localStorage, $sessionStorage, randomAvatar, serverHost, SocketIO) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  //var ChatManager = FakeChat;
+	var ChatManager = SocketIO;
+  
+  $scope.isWebView = ionic.Platform.isWebView();
+  $scope.$storage = $scope.isWebView ? $localStorage : $sessionStorage;
+  //$scope.$storage.avatar = $scope.$storage.avatar ? $scope.$storage.avatar : 'img/venkman.jpg';
+  $scope.serverHost = serverHost;
+
+  $scope.save = function () {
+    $scope.$storage.avatar = randomAvatar.getnewAvatar();
+		//$scope.add("Joined");
+    ChatManager.add({
+			message: 'Joined',
+			img: null,
+			timestamp: new Date().getTime(),
+      user: $scope.$storage.handle,
+      avatar: $scope.$storage.avatar
+		});
+		$scope.modal.hide();
+	}
+
+	$ionicModal.fromTemplateUrl('templates/account.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function (modal) {
+		$scope.modal = modal;
+		$scope.modal.show();
+	});
+
+});
