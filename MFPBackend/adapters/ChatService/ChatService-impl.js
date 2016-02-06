f8e5cb549097de1d0871153e2c8768da13292b58
Carlos Santana <csantana23@gmail.com>
2016-02-06 14:59:50 -0500

    update adapter

--- MFPBackend/adapters/ChatService/ChatService-impl.js
+++ MFPBackend/adapters/ChatService/ChatService-impl.js
@@ -2,8 +2,8 @@
 
 function getChatServiceInfo(){
   
-  //Setup a SocketIO NodeJS Server on Bluemix
-  //Do Deploy visit this url: http://ibm.biz/lab6678
+  //Setup a SocketIO NodeJS Server on Bluemix first
+  //To Deploy visit this url: http://ibm.biz/lab6678
   var chatUrl = 'https://chatservice.mybluemix.net';
   
   return {'chatUrl':chatUrl};
