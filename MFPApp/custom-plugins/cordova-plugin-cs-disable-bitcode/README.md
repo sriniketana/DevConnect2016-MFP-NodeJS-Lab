90b6ac1909b83804c1576d60a616ae1948b69b53
Carlos Santana <csantana23@gmail.com>
2016-02-07 11:36:20 -0500

    refresh bitcode plugin

--- MFPApp/custom-plugins/cordova-plugin-cs-disable-bitcode/README.md
+++ MFPApp/custom-plugins/cordova-plugin-cs-disable-bitcode/README.md
@@ -14,7 +14,11 @@ for architecture arm64 clang: error: linker command failed with exit code 1
 
 ## Installation
 
-Using cordova CLI
+Using cordova CLI from npm
+```
+cordova plugin add cordova-plugin-cs-disable-bitcode
+```
+Using cordova CLI from git 
 ```
 cordova plugin add https://github.com/csantanapr/cordova-plugin-cs-disable-bitcode
 ```
--- MFPApp/custom-plugins/cordova-plugin-cs-disable-bitcode/src/build-property.js
+++ MFPApp/custom-plugins/cordova-plugin-cs-disable-bitcode/src/build-property.js
@@ -6,16 +6,18 @@
 
 var fs = require('fs');
 var path = require('path');
+// cant use context.requireCordovaModule, the version of xcode might be too old inside cordova-lib
 var xcode = require('xcode');
 
 function addBuildProperty(context, prop, value, build_name){
   var projectRoot = path.resolve(context.opts.projectRoot);
-  var ConfigParser = context.requireCordovaModule('cordova-lib').configparser
+  var ConfigParser = context.requireCordovaModule('cordova-lib').configparser;
   var config = new ConfigParser(path.join(projectRoot, 'config.xml'));
   var projectName = config.name();
   var projectPath = path.join(projectRoot, 'platforms/ios/', projectName+'.xcodeproj','project.pbxproj');
   var myProj = xcode.project(projectPath);
   
+  //we need to use Sync because async causes problems when other plugins are handling pbxproj file
   myProj.parseSync();
   myProj.addBuildProperty(prop, value,build_name);
   fs.writeFileSync(projectPath, myProj.writeSync());
@@ -23,12 +25,13 @@ function addBuildProperty(context, prop, value, build_name){
 }
 function removeBuildProperty(context, prop, build_name){
   var projectRoot = path.resolve(context.opts.projectRoot);
-  var ConfigParser = context.requireCordovaModule('cordova-lib').configparser
+  var ConfigParser = context.requireCordovaModule('cordova-lib').configparser;
   var config = new ConfigParser(path.join(projectRoot, 'config.xml'));
   var projectName = config.name();
   var projectPath = path.join(projectRoot, 'platforms/ios/', projectName+'.xcodeproj','project.pbxproj');
   var myProj = xcode.project(projectPath);
   
+  //we need to use Sync because async causes problems when other plugins are handling pbxproj file
   myProj.parseSync();
   myProj.removeBuildProperty(prop, build_name);
   fs.writeFileSync(projectPath, myProj.writeSync());
